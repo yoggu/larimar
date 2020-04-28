6d7564cf431e1f2e4eb35083d840e24add7a242a
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2020-04-28 14:57:04 +0200

    pwa config

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,15 @@ module.exports = {
         prependData: `@import "@/styles/_variables.scss";`
       }
     }
+  }, 
+  pwa: {
+    name: 'Larimar',
+    themeColor: '#85d0e7',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
   }  
 };
