a029fcd59a9653522b54edd95f984b1900b5cc5f
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2020-05-27 17:20:28 +0200

    credits

---
+++ src/views/Credits.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="credits">
+    
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Credits',
+  components: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
