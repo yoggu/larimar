091570dc1bf4eca9863c8d6f8e9ede1a848e076d
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2020-06-02 16:04:33 +0200

    startscreen

--- src/views/StartScreen.vue
+++ src/views/StartScreen.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="start-screen">
     <div class="title">
-      <img :src="require('@/assets/images/title.png')">
+      <h1>Larimar</h1>
+      <h2 class="subtitle">sag mir, was soll ich werden</h2>
     </div>
-    <div class="stone">
+    <div class="frog">
       <router-link to="/menu">
-        <img class="" :src="require('@/assets/images/frog_sleep.gif')">
+        <!-- <img class="" :src="require('@/assets/images/frog_sleep.gif')"> -->
       </router-link>
     </div>
     <div class="start">
       <router-link to="/menu">
-      <img :src="require('@/assets/images/start.png')">
+        zum Beginnen hier klicken
       </router-link>
     </div> 
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: 'StartScreen',
@@ -29,43 +29,54 @@ export default {
 
 <style lang="scss" scoped>
 .start-screen {
-  display: grid;
-  grid-template-rows: 200px minmax(0,300px) 200px ;
-  grid-template-columns: auto minmax(auto, 768px ) auto;;
+  max-width: 768px;
+  margin: 0 auto;
   height: 100vh;
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
 
   .title {
     grid-row: 1;
     grid-column: 2;
     text-align: center;
-  }
 
-  .stone {
-    grid-row: 2;
-    grid-column: 2;
-    text-align: center;
+    h1 {
+      font-size: 60px;
+      margin-bottom: 0;
+    }
 
-    .move-stone {
-      animation: move-stone 1s infinite alternate ease-in-out;
+    .subtitle {
+      font-size: 14px;
+      margin: 0;
     }
   }
 
+  .frog {
+    background-image: url("../assets/images/frog_sleep.gif");
+    height: 100%;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+
+  }
+
   .start {
-    grid-row: 3;
-    grid-column: 2;
-    text-align: center;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 }
 
-@keyframes move-stone {
-  from {
-    transform: translateY(-10px);
-  }
-  to {
-    transform: translateY(10px);
-  }
+// @keyframes move-stone {
+//   from {
+//     transform: translateY(-10px);
+//   }
+//   to {
+//     transform: translateY(10px);
+//   }
   
-}
+// }
 
 </style>
