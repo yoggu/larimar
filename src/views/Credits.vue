7d959336f2eb727787d6f376c3c6b8bd23415203
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2020-06-04 19:04:44 +0200

    Update Credits.vue

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -5,8 +5,20 @@
     </div>
     <div class="content">
       <h1>Credits</h1>
-      <h2>Seline Schwab</h2>
-      <h2>Yannick Bättig</h2>
+      <h2>Ideation & Creation</h2>
+      <h3>Seline Schwab</h3>
+      <p>Design</p>
+      <p>Story</p>
+      <p>Illustrationen</p>
+      <p>Research</p>
+      <h3>Yannick Bättig</h3>
+      <p>Programming</p>
+      <p>Audio</p>
+      <p>Research</p>
+
+      <div>
+        <p>Besonderen Dank an unsere Mentorinnen und Mentor und das BIZ Zug für die Unterstützung</p> 
+      </div>
     </div>  
 
   </div>
